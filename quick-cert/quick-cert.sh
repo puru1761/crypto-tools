43eb64dd45d0ca465711c817cd9ea994fa889d68
Purushottam Kulkarni <puru1761@gmail.com>
2019-02-18 18:14:22 -0800

    Quick Certificates
    * Added functionality to create a directory in the ${CWD} to hold the cert/key
    * Added naming convention for the same as ${COMMON_NAME}_certs

--- quick-cert/quick-cert.sh
+++ quick-cert/quick-cert.sh
@@ -3,8 +3,10 @@
 # Add -v for viewing the cert in less pager
 # Add p12 for generating a PKCS12 Archive
 
-OPTION=$1
+# Set up any global constants here
+COMMON_NAME=$1
 EXE=$(basename $0)
+CERTS_DIR="${PWD}/${COMMON_NAME}.certs"
 
 USAGE="""
 usage: ${EXE} COMMON_NAME [options]
@@ -20,23 +22,32 @@ if [[ $# -lt 1 ]]; then
     exit
 fi
 
-COMMON_NAME=$1
 
 # Generate a self signed certificate
 # The Cert is generated for common name "0.0.0.0"
 openssl req \
 	-newkey rsa:4096 -nodes \
-	-keyout "${COMMON_NAME}".key -x509 \
+	-keyout "${CERTS_DIR}/${COMMON_NAME}".key -x509 \
 	-days 365 \
 	-subj "/C=US/ST=California/L=SVL/O=RNP/OU=Security/CN=${COMMON_NAME}" \
-	-out "${COMMON_NAME}".pem \
+	-out "${CERTS_DIR}/${COMMON_NAME}.pem" \
 
 if [[ $2 == "-v" ]]; then
-	openssl x509 -text -noout -in ${COMMON_NAME}.pem | less
+	openssl x509 \
+        -text \
+        -noout \
+        -in ${CERTS_DIR}/${COMMON_NAME}.pem | less
 fi
 
 if [[ $2 == "-p12" ]]; then
-	openssl pkcs12 -inkey ${COMMON_NAME}.key -in ${COMMON_NAME}.pem \
-		-export -out ${COMMON_NAME}.p12
-	openssl pkcs12 -in ${COMMON_NAME}.p12 -noout -info
+	openssl pkcs12 \
+        -inkey ${CERTS_DIR}/${COMMON_NAME}.key \
+        -in ${CERTS_DIR}/${COMMON_NAME}.pem \
+		-export \
+        -out ${CERTS_DIR}/${COMMON_NAME}.p12
+
+    openssl pkcs12 \
+        -in ${CERTS_DIR}/${COMMON_NAME}.p12 \
+        -noout \
+        -info
 fi
