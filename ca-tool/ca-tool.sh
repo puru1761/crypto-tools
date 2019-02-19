63a709b5eff5a05b82215776e79a884d26e6a20c
Purushottam Kulkarni <puru1761@gmail.com>
2019-02-18 17:51:35 -0800

    Comments and Documentation
    * Added comments explaining how the various directories are set up
    * Added TODOs

--- ca-tool/ca-tool.sh
+++ ca-tool/ca-tool.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+#
+# ca-tool - Tool for creating and managing a private Certificate Authority (CA)
+#
+# For usage, see help
+# Author(s):
+#
+# Purushottam Kulkarni (puru1761)
+# 
+# Copyright (c) 2019
+#
+
+# Set up any global constants here
 CMD=$1
 ROOT_CA_DIR="/root/ca"
-CA_CERTS_DIR="/cert-auth"
+DEBUG=1 # Debug constant. set to 1 to enable
 
+# Name of the program.
+# Adapts to change if user changes it
 EXE=$(basename $0)
 
+# Usage help text
+# See here for how to use the tool
 USAGE="""
 usage: ${EXE} CMD [CMD_ARGS]
 
@@ -27,6 +43,12 @@ ${EXE} ca-cert my_intermediate_ca
 ${EXE} keypair 1.1.1.1 my_intermediate_ca
 """
 
+# OpenSSL configuration used for managing Root CA.
+# This is the template used for configuring the Root CA as well as any
+# Intermediate CAs
+#
+# Taken from: https://jamielinux.com/docs/openssl-certificate-authority/appendix/root-configuration-file.html
+# Great How To on generating a certificate chain. Check it out!
 OPENSSL_CNF="""
 # OpenSSL CA configuration file.
 # Copy to '/root/ca/openssl.cnf'.
@@ -170,15 +192,22 @@ edit_root_config() {
 	sudo vim ${ROOT_CA_DIR}/openssl.cnf
 }
 
+# Create the root configuration from the hard-coded config
 create_root_config() {
 	echo "${OPENSSL_CNF}" | sudo tee ${ROOT_CA_DIR}/openssl.cnf > /dev/null
 }
 
+# Create the CA configuration
+# Patches root config with a customized patch
 create_ca_config() {
 
 	CA_NAME=$1
 	CA_DIR=$2
 
+    # This is the patch file used to generate the openssl.cnf file
+    # Customizes the CA directory as well as the certificate/key names
+    # It is derived from the Root CA's openssl.cnf
+    # TODO: Add a methodology to adjust the policy based on user preference
 	CONF_PATCH="10c10
 < dir               = /root/ca
 ---
@@ -198,48 +227,83 @@ create_ca_config() {
 ---
 > policy            = policy_loose"
 
-	sudo cp ${ROOT_CA_DIR}/openssl.cnf ${CA_DIR}/
+    # Copy over the root CA's openssl.cnf
+	sudo rsync -a ${ROOT_CA_DIR}/openssl.cnf ${CA_DIR}/
+
+    # Generate the patch file for Root CA's openssl.cnf
 	echo "${CONF_PATCH}" | sudo tee ${CA_DIR}/openssl.patch > /dev/null
+
+    # Patch CA's openssl.cnf with the gerated patch
 	sudo patch ${CA_DIR}/openssl.cnf ${CA_DIR}/openssl.patch > /dev/null
 
-	sudo vim ${CA_DIR}/openssl.cnf
+    # Do any debug actions here
+    if [[ ${DEBUG} == 1 ]]; then
+        # We are in debug mode, open the CA's openssl.cnf for user editing
+	    sudo vim ${CA_DIR}/openssl.cnf
+    fi
+
+    # Clean up the patch file and the original openssl.cnf
+    sudo rm  -f ${CA_DIR}/openssl.cnf.orig ${CA_DIR}/openssl.patch
 
 }
 
+#
+# Initialize the root CA's env
+# 
+# Create required directories and setup their perms.
+# Also initialize the serial list for the Root and an index for issued Certs
+#
 init_trust_root() {
 
+    # Delete the Original root ca.
+    # Only one root ca can exist at a time
 	sudo rm -rf ${ROOT_CA_DIR}
 
+    # Set up the directories to work from
 	sudo mkdir -p ${ROOT_CA_DIR}
 	sudo mkdir -p ${ROOT_CA_DIR}/{certs,crl,newcerts,private}
 	
+    # Set up permissions for the directory to hold private keys
 	sudo chmod 700 ${ROOT_CA_DIR}/private
+
+    # Initialized the issued cert index
 	sudo touch ${ROOT_CA_DIR}/index.txt
+
+    # Initialize the serial number
 	echo 1000 | sudo tee ${ROOT_CA_DIR}/serial > /dev/null
 
 	create_root_config
 }
 
+#
+# Initialize the trusted CA's env
+# Also generates the CA's config using the CA_NAME passed in
+#
 init_trust_authority() {
 
 	CA_NAME=$1
 	TRUST_CHAIN=$2
 
+    # Generate the Name of the CA directory
 	if [[ ${TRUST_CHAIN} == "" ]]; then
 		CA_DIR=${ROOT_CA_DIR}/${CA_NAME}
 	else
 		CA_DIR=${ROOT_CA_DIR}/${TRUST_CHAIN}/${CA_NAME}
 	fi
 	
+    # Make directories to work from
 	sudo mkdir -p ${CA_DIR}
 	sudo mkdir -p ${CA_DIR}/{certs,crl,csr,newcerts,private}
 	
+    # Set the permisions for the CA's directory
 	sudo chmod 700 ${CA_DIR}/private
 	sudo touch ${CA_DIR}/index.txt
-	echo 1000 | sudo tee ${CA_DIR}/serial > /dev/null
 
+    # Initialize the certificate serial numbers and crl numbers
+	echo 1000 | sudo tee ${CA_DIR}/serial > /dev/null
 	echo 1000 | sudo tee ${CA_DIR}/crlnumber > /dev/null
 
+    # Create the CA's configuration using the CA's name
 	create_ca_config ${CA_NAME} ${CA_DIR}
 }
 
@@ -285,11 +349,18 @@ gen_root_keypair() {
 		| less
 }
 
+# Generate a CA Keypair under ${ROOT_CA_DIR}
+# For a nested CA this is generated under ${ROOT_CA_DIR}/${NESTED_CA_LIST}/
+#
+# Can generate an infinitely nested CA tree and a leaf server certificate which
+# can be verified up the chain
 gen_ca_keypair() {
 
 	CA_NAME=$1
 	TRUST_ROOT=$2
 
+    # Set up the CA for this CA
+    # This information is required to sign the certificate for the CA
 	CA_CHAIN_DIR=${ROOT_CA_DIR}/${TRUST_ROOT}
 	if [[ ${TRUST_ROOT} == "" ]]; then
 		CA_DIR=${ROOT_CA_DIR}/${CA_NAME}
@@ -367,8 +438,8 @@ gen_ca_keypair() {
 	sudo chmod 444 ${CA_DIR}/certs/fullchain.pem
 }
 
-# Create your very own Root Certificate Authority
-
+# Create your very own Server Certificate
+#
 # Create a Device Certificate for each domain,
 # such as example.com, *.example.com, awesome.example.com
 # NOTE: You MUST match CN to the domain name or ip address you want to use
@@ -383,6 +454,8 @@ gen_keypair() {
 
 	GROUP=$(id -g -n ${USER})
 
+    # Set up the Certificate subject
+    # TODO: Create a way to provide consistent subjects
 	NAME="${COMMON_NAME}.$(hostname)-${USER}"
 	SUBJ="/C=US"
 	SUBJ="${SUBJ}/ST=California"
@@ -394,10 +467,12 @@ gen_keypair() {
 	# make directories to work from
 	mkdir -p ${PWD_CERTS_DIR}/tmp
 	
+    # Generate the server private key
 	sudo openssl genrsa \
 		-out ${CERTS_DIR}/private/${COMMON_NAME}.key \
 		4096
 
+    # Set up server private key permissions
 	sudo chmod 400 ${CERTS_DIR}/private/${COMMON_NAME}.key
 	sudo cp ${CERTS_DIR}/private/${COMMON_NAME}.key ${PWD_CERTS_DIR}/
 	
@@ -413,12 +488,13 @@ gen_keypair() {
 		-out ${CERTS_DIR}/csr/${COMMON_NAME}_csr.pem \
 		-subj "${SUBJ}"
 
+    # Create and store the Server CSR with proper perms
 	sudo cp ${CERTS_DIR}/csr/${COMMON_NAME}_csr.pem ${PWD_CERTS_DIR}/tmp/
 	sudo chown ${USER}:${GROUP} ${PWD_CERTS_DIR}/tmp/${COMMON_NAME}_csr.pem
 	sudo chmod 444 ${PWD_CERTS_DIR}/tmp/${COMMON_NAME}_csr.pem
 
-	# Sign the request from Device with your Root CA
-	# -CAserial ${CERTS_DIR}/ca/my-root-ca.srl
+	# Sign the request from Device with your Trusted CA
+	# Use the extension server_cert from the Trusted CA's openssl.cnf
 	sudo openssl ca \
 		-config ${CERTS_DIR}/openssl.cnf \
 		-extensions server_cert \
@@ -430,14 +506,17 @@ gen_keypair() {
 
 	sudo chmod 444 ${CERTS_DIR}/certs/${COMMON_NAME}.cert.pem
 
+    # Set up the server certificate in ${CWD} and set correct perms
 	sudo cp ${CERTS_DIR}/certs/${COMMON_NAME}.cert.pem ${PWD_CERTS_DIR}/
 	sudo chown ${USER}:${GROUP} ${PWD_CERTS_DIR}/${COMMON_NAME}.cert.pem
 
+    # View the server cert in a less pager
 	openssl x509 \
 		-noout \
 		-text \
 		-in ${PWD_CERTS_DIR}/${COMMON_NAME}.cert.pem | less
 
+    # Verify the server certificate
 	sudo openssl verify \
 		-CAfile ${CERTS_DIR}/certs/fullchain.pem \
 		${CERTS_DIR}/certs/${COMMON_NAME}.cert.pem
@@ -461,10 +540,14 @@ gen_keypair() {
 		> ${PWD_CERTS_DIR}/${DEVID}/fullchain.pem
 }
 
+# Generate PKCS12 archive for a Cert/Key/CA.
+# Used as an option, not enabled by default
 gen_pkcs12_archive() {
+
 	CERTS_DIR="${PWD}/certs"
 	CERT_NAME="$1"
 
+    # Generate a pkcs12 archive for the pointed cert
 	openssl pkcs12 \
 		-inkey ${CERTS_DIR}/${CERT_NAME}.key \
 		-in ${CERTS_DIR}/${CERT_NAME}.cert.pem \
@@ -480,15 +563,17 @@ fi
 
 # Parse the commandline args
 case "$1" in
+    
+    # Generate a server Certificate/Key Pair
 	keypair)
 		if [[ $# -lt 2 ]]; then
 			echo "error(keypair): 'COMMON_NAME' required"
-			exit
+			exit 1
 		fi
 
 		if [[ $# -lt 3 ]]; then
 			echo "error(keypair): 'TRUST_CHAIN' required"
-			exit
+			exit 1
 		fi
 
 		gen_keypair $2 $3
@@ -500,10 +585,12 @@ case "$1" in
 			echo "Generated PKCS12 Archive for '$2'"
 		fi
 		;;
+
+    # Generate an Intermediate CA cert/key pair
 	ca-cert)
 		if [[ $# -lt 2 ]]; then
 			echo "error(ca-cert) 'CA_NAME' required"
-			exit
+			exit 1
 		fi
 		
 		init_trust_authority $2 $3
@@ -514,6 +601,8 @@ case "$1" in
 			echo "Using Trusted Intermediate CA(s): '$3'"
 		fi
 		;;
+
+    # Generate a Root CA (Note, will erase previous root CA, if it exists)
 	root-ca)
 		init_trust_root
 		gen_root_keypair
